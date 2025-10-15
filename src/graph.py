6c1e1326fb366cad266809ca9103f3f3aaba59a9
Zefan Huang <zefanhuang@Zefans-Mac-mini.local>
2025-10-15 14:53:33 -0700

    I put one more gragh

--- src/graph.py
+++ src/graph.py
@@ -4,12 +4,11 @@ import matplotlib.pyplot as plt
 df = pd.read_csv('output/analysis_result.csv')
 
 def plot(df):
-    plt.figure(figsize=(10, 5))
+    plt.figure(figsize=(20, 5))
 
-    # Plot a single id (change selected_id to the id you want)
-    selected_id = 1
-    patient_data = df[df['fitmri_id'] == selected_id]
-    plt.plot(patient_data['measured_date'], patient_data['hourly_avg_steps']) ## draw it with date and avf steps
+    for id in df['fitmri_id'].unique():   #id = 1,2,3 ... 26
+        patient_data = df[df['fitmri_id'] == id]    ## whole row of cur id
+        plt.plot(patient_data['measured_date'], patient_data['hourly_avg_steps']) ## draw it with date and avf steps?
 
     plt.title('Hourly Average Steps (All Patients)')
     plt.xlabel('Date')
@@ -22,15 +21,16 @@ plot(df)
 
 def one_patient_plot(df, patient_id):
 
-    plt.figure(figsize=(10, 5))
+    plt.figure(figsize=(20, 5))
     patient_data = df[df['fitmri_id'] == patient_id]
     plt.plot(patient_data['measured_date'], patient_data['hourly_avg_steps'])
 
-    plt.title('Hourly Average Steps (Selceted Patients)')
+    plt.title('Hourly Average Steps, ' + 'Patient ID:' + str(patient_id))
     plt.xlabel('Date')
     plt.ylabel('Hourly Average Steps')
     plt.grid(True)
-    plt.tight_layout()
+    #plt.tight_layout()
     plt.show()
 
-one_patient_plot(df, patient_id = 1) ### guys, select your patient id here :) hehehe
+one_patient_plot(df, patient_id = 20)   ### guys, select your patient id here :) hehehe
+## Just remember, there are Missing IDs: [6, 7, 11, 14, 16, 21] and IDs go from 1 to 26.
